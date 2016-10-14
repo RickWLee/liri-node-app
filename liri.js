4c116d3da0b08ebfe6122490a0479aa3c6e6fad7
Weihao Rick Lee <kenchi133@hotmail.com>
2016-10-14 08:53:49 -0500

    fixed twitter communication

--- liri.js
+++ liri.js
@@ -1,7 +1,21 @@
-var twitt = require('./keys.js');
-console.log("Twitt");
-console.log(twitt.twitterKeys);
 
-client.get('search/tweets', {q: 'Rick_W_Lee'}, function(error, tweets, response) {
-   console.log(tweets);
-});
+var Twitter=require('twitter');
+var twittkey = require('./keys.js');
+// console.log(twittkey.twitterKeys);
+var keys=twittkey.twitterKeys;
+// console.log(keys);
+var client = new Twitter({
+	keys
+})
+console.log(client);
+
+// client.get('favorites/list', function(error, tweets, response) {
+//   if(error) throw error;
+//   console.log(tweets);  // The favorites. 
+//   console.log(response);  // Raw response object. 
+// });
+// client.post('statuses/update', {status: 'I Love Twitter'},  function(error, tweet, response) {
+//   if(error) throw error;
+//   console.log(tweet);  // Tweet body. 
+//   console.log(response);  // Raw response object. 
+// });
